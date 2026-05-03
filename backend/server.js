7fe82f9f6c642f0670548b93505ea33d067998c0
Kunal Kushwah <146961274+kunnu077@users.noreply.github.com>
2026-05-03 17:05:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal server error', error: err.message });
 });
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
